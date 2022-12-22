519fdb3e9f733ef63fd43c318a47590cebc4b035
David_Motari <davemotari22@gmail.com>
2022-12-22 10:38:17 +0300

    Task 6 correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filaName) {
-  return [{ status: 'fulfilled', value: await signUpUser(filaName, lastName).then((objct) => objct) }, { status: 'rejected', value: await uploadPhoto(filaName).catch((error) => error.toString()) }];
+  return [{ status: 'fulfilled', value: await signUpUser(firstName, lastName).then((objct) => objct) }, { status: 'rejected', value: await uploadPhoto(filaName).catch((error) => error.toString()) }];
 }

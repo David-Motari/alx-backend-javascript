2871206c48739259c9137aaadf584e9598f152d6
David_Motari <davemotari22@gmail.com>
2022-12-22 06:01:45 +0300

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((objct) => console.log(`${objct[0].body} ${objct[1].firstName} ${objct[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }

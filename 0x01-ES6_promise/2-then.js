34ee117b6bd7ace85650b6535380562351df9fce
David_Motari <davemotari22@gmail.com>
2022-12-22 05:31:01 +0300

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 function handleResponseFromAPI(promise) {
   const resp = {
     status: 200,
-    body: "success",
-  }
+    body: 'success',
+  };
   return promise.then(() => resp).catch(() => new Error()).finally(() => {
-    console.log("Got a response from the API");
-    });
+    console.log('Got a response from the API');
+  });
 }
 
-export default handleResponseFromAPI(promise);
+export default handleResponseFromAPI;

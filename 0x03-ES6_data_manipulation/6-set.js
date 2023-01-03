0e9b0deadb5842e665a635eb7d302cbee5e4ed7c
David_Motari <davemotari22@gmail.com>
2023-01-03 16:36:44 +0300

    Task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,11 @@
+export default function setFromArray(arr) {
+  if (!(Array.isArray(arr))) {
+    return []
+  }
+  const set1 = new Set();
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < arr.length; i++) {
+    set1.add(arr[i]);
+  }
+  return set1;
+}

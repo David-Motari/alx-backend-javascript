2296c85dbbb844345ad15cebbfcd1a22625c562d
David_Motari <davemotari22@gmail.com>
2023-01-04 04:28:57 +0300

    Task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+export default function groceriesList() {
+  const newMap = new Map();
+  return newMap.set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+}

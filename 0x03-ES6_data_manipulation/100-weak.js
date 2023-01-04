5b1677c6936a45e17a8283f614766d9c19229e4a
David_Motari <davemotari22@gmail.com>
2023-01-04 05:04:19 +0300

    Task 11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+const weakMap = new WeakMap();
+const queryAPI = (endpoint) => {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
+};
+export { queryAPI, weakMap };

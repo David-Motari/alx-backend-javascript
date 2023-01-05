2f9a3b978b7d72155f89360e36571cf0ed8bcd5d
David_Motari <davemotari22@gmail.com>
2023-01-05 12:12:54 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0-add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    type annoated variables that return a sum of the two variables
+    """
+    return a + b

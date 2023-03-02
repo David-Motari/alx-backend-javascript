bcc7ef33eaf69845e3822c1d5b87687c91775271
David-Motari <davemotari22@gmail.com>
2023-03-02 16:43:59 +0300

    Express basic server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+}).listen(port, '127.0.0.1');

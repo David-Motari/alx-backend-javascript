a4d78277b773d70753abe2e733f66e2b50a534ee
David Motari <david_motari@Xubuntu.myguest.virtualbox.org>
2023-03-02 07:58:56 +0300

    read file synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(file_path) {
+  try {
+    let content = fs.readFileSync(file_path, 'utf-8').toString().split('\n');
+    content = content.slice(1, content.length - 1);
+    console.log(`Number of students: ${content.length}`);
+    const fields = {};
+    for (const row of content) {
+      const student = row.split(',');
+      if (!fields[student[3]]) {
+        fields[student[3]] = [];
+      }
+      fields[student[3]].push(student[0]);
+    }
+    for (const field in fields) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+
+  }
+  catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;

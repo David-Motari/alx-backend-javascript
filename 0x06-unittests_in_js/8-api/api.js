fd4256be3730bf445a3ba033860a4ffd0f5add5a
David Motari <david@Xubuntu.myguest.virtualbox.org>
2023-03-07 16:47:03 +0300

    Correction

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.argv[1] || 7865;
+const port = 7865;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system')
+  res.end('Welcome to the payment system')
 })
 
 app.listen(port, () => {
